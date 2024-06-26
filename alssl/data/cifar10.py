113c1bc84e8f233158dc9cbfae3c6b4e842de292
Fyaushev <yaushev@phystech.com>
2024-06-26 17:08:25 +0300

    updated datasets

--- alssl/data/cifar10.py
+++ alssl/data/cifar10.py
@@ -1,11 +1,14 @@
 from pathlib import Path
 from typing import Iterable
 
-import torch
-from torch.utils.data import random_split
+import lightning as L
+import numpy as np
+from sklearn.model_selection import train_test_split
+from torch.utils.data import Subset, random_split
 from torch.utils.data.dataloader import DataLoader
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
+from dpipe.io import save
 
 DATA_PATH = Path("/shared/projects/active_learning/data/cifar10")
 
@@ -27,12 +30,10 @@ def get_data_loaders(
     val_size: int | float,
     *,
     batch_size: int = 128,
-    random_seed: int = 42,
     num_workers: int = 4,
     transform=dino_transform,
     shuffle=False,
 ) -> Iterable[DataLoader]:
-    torch.manual_seed(random_seed)
 
     train_ds = ImageFolder(DATA_PATH / "train", transform=transform)
     test_ds = ImageFolder(DATA_PATH / "test", transform=transform)
@@ -44,9 +45,70 @@ def get_data_loaders(
     train_ds, val_ds = random_split(train_ds, [train_size, val_size])
 
     train_dl = DataLoader(
-        train_ds, batch_size, shuffle=shuffle, num_workers=num_workers
+        train_ds, batch_size, num_workers=num_workers, shuffle=shuffle
     )
-    val_dl = DataLoader(val_ds, batch_size, num_workers=num_workers)
-    test_dl = DataLoader(test_ds, batch_size, num_workers=num_workers)
+    val_dl = DataLoader(val_ds, batch_size, num_workers=num_workers, shuffle=False)
+    test_dl = DataLoader(test_ds, batch_size, num_workers=num_workers, shuffle=False)
 
     return train_dl, val_dl, test_dl
+
+
+class CIFAR10LightningDataModule(L.LightningDataModule):
+    def __init__(
+        self,
+        batch_size,
+        *,
+        trains_size=950,
+        val_size=50,
+        train_transform=dino_transform,
+        test_transform=dino_transform,
+        random_state=0,
+        num_workers=8,
+    ):
+        super().__init__()
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+        train_dataset = ImageFolder(DATA_PATH / "train", transform=train_transform)
+        test_dataset = ImageFolder(DATA_PATH / "test", transform=test_transform)
+
+        train_ids, val_ids = train_test_split(
+            np.arange(len(train_dataset)),
+            train_size=trains_size,
+            test_size=val_size,
+            random_state=random_state,
+            stratify=train_dataset.targets,
+        )
+
+        save(train_ids, "train_ids.json")
+        save(val_ids, "val_ids_ids.json")
+
+        self.train_dataset = Subset(train_dataset, train_ids)
+        self.val_dataset = Subset(train_dataset, val_ids)
+
+        self.val_dataset.transform = test_transform
+        self.test_dataset = test_dataset
+
+    def train_dataloader(self):
+        return DataLoader(
+            self.train_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def val_dataloader(self):
+        return DataLoader(
+            self.val_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+    def test_dataloader(self):
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
