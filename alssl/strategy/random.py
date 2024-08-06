0a3490e24910e51ab39c81efc87a41a4d58017d9
Fyaushev <yaushev@phystech.com>
2024-08-06 14:57:29 +0300

    comments

--- alssl/strategy/random.py
+++ alssl/strategy/random.py
@@ -6,6 +6,13 @@ from .base import BaseStrategy
 
 
 class RandomStrategy(BaseStrategy):
+    """
+    Random Strategy for Active Learning.
+
+    This strategy randomly selects a specified number of unlabeled data points
+    from the dataset to be labeled next, based on the given budget.
+    """
+
     def select_ids(self, model: nn.Module, dataset: ALDataModule, budget: int):
         train_ids = dataset.train_ids
         val_ids = dataset.val_ids
