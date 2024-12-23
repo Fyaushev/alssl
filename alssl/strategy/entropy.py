d09c1aa8fbb0a7f28f80dc2260e35a0876defad2
Daria Nogina <d.nogina@ira-labs.com>
2024-12-23 16:17:32 +0300

    modify base strategies

--- alssl/strategy/entropy.py
+++ alssl/strategy/entropy.py
@@ -9,6 +9,9 @@ from .utils import predict
 
 
 class EntropyStrategy(BaseStrategy):
+    def __init__(self, random_proportion: float = .9):
+        self.random_proportion = random_proportion
+
     def select_ids(self, model: nn.Module, dataset: ALDataModule, budget: int, _):
         
         unlabeled_dataset = dataset.unlabeled_dataloader()
@@ -20,7 +23,14 @@ class EntropyStrategy(BaseStrategy):
             scoring_function=self.scoring_function)
 
         unlabeled_ids = dataset.get_unlabeled_ids()
-        return np.array(unlabeled_ids)[np.argsort(scores)][:budget].tolist()
+
+        entropy_selected_ids = np.array(unlabeled_ids)[np.argsort(scores)][:int(budget * (1-self.random_proportion))].tolist()
+
+        unselected_ids = list(set(unlabeled_ids) ^ set(entropy_selected_ids))
+
+        random_selected_ids = np.random.choice(unselected_ids, size=int(budget * self.random_proportion), replace=False).tolist()
+
+        return entropy_selected_ids + random_selected_ids
 
     def scoring_function(self, gt, pred, embeddings):
         """
--- alssl/strategy/golden_rule.py
+++ alssl/strategy/golden_rule.py
@@ -1,5 +1,7 @@
 import numpy as np
 import torch
+from scipy.special import softmax
+from sklearn.metrics import log_loss
 from torch import nn
 
 from ..data.base import ALDataModule
@@ -8,6 +10,9 @@ from .utils import predict
 
 
 class GRStrategy(BaseStrategy):
+    def __init__(self, inverse: bool = False):
+        self.inverse = inverse
+    
     def select_ids(self, model: nn.Module, dataset: ALDataModule, budget: int, _):
         
         unlabeled_dataset = dataset.unlabeled_dataloader()
@@ -18,8 +23,9 @@ class GRStrategy(BaseStrategy):
             scoring="individual", 
             scoring_function=self.scoring_function)
 
+        scores = scores if self.inverse else -scores
         unlabeled_ids = dataset.get_unlabeled_ids()
-        return np.array(unlabeled_ids)[np.argsort(-scores)][:budget].tolist()
+        return np.array(unlabeled_ids)[np.argsort(scores)][:budget].tolist()
 
     def scoring_function(self, gt, pred, embeddings):
         return nn.functional.cross_entropy(torch.Tensor(pred), gt.cpu().long(), reduction='none').numpy()
