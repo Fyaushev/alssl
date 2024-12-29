dcfc917c0121606cf04e56dade980191b5e72f77
Daria Nogina <d.nogina@ira-labs.com>
2024-12-30 00:34:04 +0300

    minor fix

--- alssl/strategy/alssl/utils.py
+++ alssl/strategy/alssl/utils.py
@@ -46,6 +46,8 @@ def load_or_compute(filepaths, compute_fn, to_save:bool = False, *args, **kwargs
         list of np.ndarray: Loaded or computed data.
     """
     if all(Path(fp).exists() for fp in filepaths):
+        if len(filepaths) == 1:
+            return np.load(filepaths[0])
         return [np.load(fp) for fp in filepaths]
     
     # Compute data and save to all files
