3d38eed0b4d2edef39bef2f0640866817c3e19fa
Daria Nogina <d.nogina@ira-labs.com>
2025-01-09 00:39:32 +0300

    add datasets

---
+++ alssl/data/img_classification/breastmnist.py
@@ -0,0 +1,56 @@
+from pathlib import Path
+
+import numpy as np
+from medmnist import INFO, BreastMNIST
+from torchvision import transforms
+
+transform_train = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.Resize((224, 224), antialias=True),
+        transforms.RandomRotation(10),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+transform_test = transforms.Compose(
+    [
+        transforms.ToTensor(), 
+        transforms.Resize((224, 224), antialias=True),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+
+def get_num_classes():
+    len(INFO["breastmnist"]["label"])
+    return 2
+
+
+class BreastMNISTDataset(BreastMNIST):
+    def __init__(self, root, split, download, size=224, as_rgb=True):
+        super().__init__(
+            root=root, split=split, download=download, size=size, as_rgb=as_rgb,
+        )
+        self.targets = self.labels.ravel()
+
+    def __getitem__(self, index):
+        # image = np.moveaxis(self.imgs[index], -1, 0)  # [224, 224, 3] to [3, 224, 224]
+        image = self.imgs[index]
+
+        image_3ch = np.zeros( ( np.array(image).shape[0], np.array(image).shape[1], 3 ) )
+        image_3ch[:,:,0] = image
+        image_3ch[:,:,1] = image
+        image_3ch[:,:,2] = image
+
+        label = self.labels[index][0]  # [label] to label
+        return image_3ch.astype(np.float32), label
+
+def get_dataset(
+    subset="train", data_path=Path("/shared/projects/active_learning/breastmnist")
+):
+    assert subset in ["train", "test"]
+    transform = transform_train if subset == "train" else transform_test
+    ds = BreastMNISTDataset(root=data_path, split=subset, download=True)
+    return ds, transform
---
+++ alssl/data/img_classification/dermamnist.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+
+import numpy as np
+from medmnist import INFO, DermaMNIST
+from torchvision import transforms
+
+transform_train = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.Resize((224, 224), antialias=True),
+        transforms.RandomRotation(10),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+transform_test = transforms.Compose(
+    [
+        transforms.ToTensor(), 
+        transforms.Resize((224, 224), antialias=True),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+
+def get_num_classes():
+    len(INFO["dermamnist"]["label"])
+    return 7
+
+
+class DermaMNISTDataset(DermaMNIST):
+    def __init__(self, root, split, download, size=224):
+        super().__init__(
+            root=root, split=split, download=download, size=size
+        )
+        self.targets = self.labels.ravel()
+
+    def __getitem__(self, index):
+        # image = np.moveaxis(self.imgs[index], -1, 0)  # [224, 224, 3] to [3, 224, 224]
+        image = self.imgs[index]
+        label = self.labels[index][0]  # [label] to label
+        return image, label
+
+def get_dataset(
+    subset="train", data_path=Path("/shared/projects/active_learning/dermamnist")
+):
+    assert subset in ["train", "test"]
+    transform = transform_train if subset == "train" else transform_test
+    ds = DermaMNISTDataset(root=data_path, split=subset, download=True)
+    return ds, transform
