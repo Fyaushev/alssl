017b4f173f10a080fe09be3842bdd22ce7164769
Daria Nogina <d.nogina@ira-labs.com>
2025-02-12 18:52:41 +0300

    add meta-neighbors

--- alssl/strategy/kmeans.py
+++ alssl/strategy/kmeans.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.cluster.vq import vq
 from scipy.special import softmax
 from sklearn.cluster import KMeans
+from sklearn.neighbors import NearestNeighbors
 from torch import nn
 from tqdm import tqdm
 
@@ -54,7 +55,62 @@ def calculate_nn_scores(model, dataset, almodel, num_neighbours=250, metric="cos
     
     return scores, kmeans_finetuned, neighbors_finetuned
 
-def calculate_umap_scores(model, dataset, almodel, num_neighbours=250, metric="cosine", load_from_prev_iter=False):
+
+def calculate_nn_clust_scores(model, all_ids, dataset, almodel, inverse, cluster_curr, num_classes, num_classes_scale=2, num_neighbours=5, metric="cosine", comb_score=False, load_from_prev_iter=False):
+    """
+    Calculate nearest-neighbor-based scores and return neighbors for further operations.
+    """
+    # Load previous model if required
+    prev_model = almodel.get_lightning_module()(**almodel.get_hyperparameters())
+    if get_current_iteration() and load_from_prev_iter:
+        prev_model.load_state_dict(get_previous_interation_state_dict())
+
+    # Compute embeddings and neighbors for original and finetuned models
+    _, _, e0 = predict(
+        prev_model,
+        dataset.unlabeled_dataloader(), 
+        scoring="none", desc="original prediction")
+    
+    _, _, e1 = predict(
+        model,
+        dataset.unlabeled_dataloader(), 
+        scoring="none", desc="finetuned prediction")
+    
+    def _run_kmeans(embeddings, _num_classes):
+        kmeans = KMeans(n_clusters=_num_classes, n_init="auto").fit(embeddings)
+        kmeans_labels = kmeans.predict(embeddings)
+        centroids = kmeans.cluster_centers_
+        closest, distances_to_centroids = vq(embeddings, centroids)
+        return kmeans_labels, distances_to_centroids
+    
+    if cluster_curr:
+        kmeans_labels, distances_to_centroids = _run_kmeans(e1, num_classes*num_classes_scale)
+    else:
+        kmeans_labels, distances_to_centroids = _run_kmeans(e0, num_classes*num_classes_scale)
+        
+    kmeans_centroids = []
+    for cluster in np.unique(kmeans_labels):
+        cluster_inds = np.argwhere(kmeans_labels == cluster).ravel()
+        kmeans_centroids += [int(cluster_inds[np.argmin(distances_to_centroids[cluster_inds])])]
+
+    neigh = NearestNeighbors(n_neighbors=num_neighbours+1, metric=metric, n_jobs=-1)
+
+    neigh.fit(X=e1[kmeans_centroids])
+    nn_finetuned = neigh.kneighbors(X=e1[kmeans_centroids], return_distance=False)[:, 1:]
+
+    neigh.fit(X=e0[kmeans_centroids])
+    nn_original = neigh.kneighbors(X=e0[kmeans_centroids], return_distance=False)[:, 1:]
+
+    nn_scores = np.array([
+        len(set(orig) & set(finetuned)) for orig, finetuned in zip(nn_original, nn_finetuned)
+    ])
+    if inverse:
+        nn_scores = -nn_scores
+
+    return all_ids[np.array(kmeans_centroids)[np.argsort(nn_scores)][:num_classes]]
+
+
+def calculate_umap_scores(model, dataset, almodel, num_neighbours=250, num_neighbours_umap=50, metric="cosine", load_from_prev_iter=False):
     """
     Calculate nearest-neighbor-based scores and return neighbors for further operations.
     """
@@ -77,8 +133,8 @@ def calculate_umap_scores(model, dataset, almodel, num_neighbours=250, metric="c
 
         combined_neighbours = np.array(list(set(nn_original) & set(nn_finetuned)))
 
-        graph_original = construct_graph(e0[combined_neighbours, :], num_neighbours)
-        graph_finetuned = construct_graph(e1[combined_neighbours, :], num_neighbours)
+        graph_original = construct_graph(e0[combined_neighbours, :], num_neighbours_umap)
+        graph_finetuned = construct_graph(e1[combined_neighbours, :], num_neighbours_umap)
 
         ce = - graph_original * np.log(graph_finetuned + 0.01) - (1 - graph_original) * np.log(1 - graph_finetuned + 0.01)
 
@@ -98,7 +154,7 @@ class KMeansStrategy(BaseStrategy):
     """
     Random sampling of initial ids
     """
-    def __init__(self, num_classes: int, samples_per_class: int = 1, is_random: bool = False, scoring=None, num_neighbours=None, num_neighbours_nms=None, comb_score=False, inverse=False, nms=True, cluster_curr=True):
+    def __init__(self, num_classes: int, samples_per_class: int = 1, is_random: bool = False, scoring=None, num_neighbours=None, num_neighbours_nms=None, comb_score=False, inverse=False, nms=True, cluster_curr=True, num_classes_scale=None):
         self.num_classes = num_classes
 
         self.samples_per_class = samples_per_class
@@ -112,11 +168,14 @@ class KMeansStrategy(BaseStrategy):
         self.inverse = inverse
         self.nms = nms
         self.cluster_curr = cluster_curr
+        self.num_classes_scale = num_classes_scale
         if self.scoring is not None and self.is_random:
             raise ValueError('Poor KMeans setup, check `scoring` and `is_random` parameters.')
 
     def select_ids(self, model: nn.Module, dataset: ALDataModule, budget: int, almodel: BaseALModel, *args) -> list:
         all_ids = np.array(dataset.get_unlabeled_ids())
+        if self.scoring == 'nn_clust':
+            return calculate_nn_clust_scores(model, all_ids, dataset, almodel, self.inverse, self.cluster_curr, self.num_classes, self.num_classes_scale, self.num_neighbours)
         
         def _predict_unlabeled():
             m = model if self.cluster_curr else almodel.get_lightning_module()(**almodel.get_hyperparameters())
