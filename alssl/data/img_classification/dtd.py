3af9b7668bbce381c8a351f4cf6e47ebb477c6ce
Daria Nogina <d.nogina@ira-labs.com>
2025-04-17 10:37:30 +0300

    dtd dataset

---
+++ alssl/data/img_classification/dtd.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+import torchvision
+from torchvision import transforms
+
+transform_train = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.Resize((224, 224), antialias=True),
+        transforms.RandomRotation(10),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+transform_test = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize((224, 224), antialias=True),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    ]
+)
+
+
+def get_num_classes():
+    return 47
+
+
+def get_dataset(
+    subset="train", data_path=Path("/shared/projects/active_learning/dtd")
+):
+    assert subset in ["train", "test"]
+    transform = transform_train if subset == "train" else transform_test
+    ds = torchvision.datasets.DTD(
+            root=data_path, split=subset, download=True
+        )
+    ds.targets = ds._labels
+    return (
+        ds,
+        transform,
+    )
+    
